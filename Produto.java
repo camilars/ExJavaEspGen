90703544a1e96f756c50804ed1bdd018138deaed
camilars <camila.rs122@gmail.com>
2018-06-06 09:00:47 -0300

    incluido App

--- Produto.java
+++ Produto.java
@@ -1,38 +1,63 @@
-class Produto{
+abstract class Produto{
 	public String fabricante;
 	public String modelo;
 	public String descricao;
-	public flaot preco;
+	public double preco;
 }
 
-class Televisao{
+abstract class Eletronico extends Produto{
+	public int voltagem;
+	public int anoLancamento;
+	public int consumoEnergia;
 
 }
-
-class Refrigerador{
-
-}
-
-class ArCondicionado{
-
-}
-
-class Roupeiro{
+class Televisao extends Eletronico{
+	protected int polegadas;
 
 }
 
-class Informatica extends Produto{
-	public String memoria;
-	public String capacidadeHD;
+class Refrigerador extends Eletronico{
+	protected int capacidade;
+	protected boolean duplex;
+	protected boolean frostFree;
+}
+
+class ArCondicionado extends Eletronico{
+	protected float potencia;
+	protected boolean split;
+}
+
+class Roupeiro extends Produto{
+	protected float altura;
+	protected float largura;
+	protected float profundidade;
+	protected String[] materiais;
+}
+
+abstract class Computador extends Produto{
+	public int memoria;
+	public int capacidadeHD;
 	public String processador;
 	public String placaDeVideo;
 }
 
-class Computador extends Informatica{
-
+class Notebook extends Computador{
+	protected int polegadas;
+	protected int duracaoDaBateria;
 }
 
-class Notebook extends Informatica{
-	public flaot polegadas;
-	public String duracaoDaBateria;
+class App{
+	public static void main(String[] args) {
+		Produto carrinho[] = new Produto[10];
+
+	carrinho[0] = new ArCondicionado();
+	carrinho[1] = new Notebook();
+	carrinho[2] = new Roupeiro();
+	carrinho[3] = new Televisao();
+
+	for (int i = 0; i < carrinho.length; i++) {
+		System.out.println("Produto" + i + ":" + carrinho)
+	}
+
+	}
 }
